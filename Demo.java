cec3bad51f6acc98720739d2388ac97a952bbbcf
lglg <2795928798@qq.com>
2019-03-28 18:40:34 +0800

    Change Two Files

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 Public class Hello{
 	public static void main(String[] str){
 		System.out.println("Hello Demo");
-		
+		System.out.println("Hello NewLine");
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 Public class Hello{
 	public static void main(String[] str){
 		System.out.println("Hello lglg");
+		System.out.println("Hello lg");
 	}
 }
